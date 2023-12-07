bb092f6f5cf78865fdce192a4e2c7ae5fbf9abe0
Mirabbos995 <jahonaliyevmirabbos621@gmail.com>
2023-12-07 16:28:42 +0300

    dehb

---
+++ main.py
@@ -0,0 +1,81 @@
+names = ["Ali","Vali","Hasan","Husan","Olim"]          # 1- misol
+for name in names:
+    if name == "Ali":
+        print(f"Salom {name}")
+    elif name == "Vali":
+        print(f"Salom {name}")
+    elif name == "Hasan":
+        print(f"Salom {name}")
+    elif name == "Husan":
+        print(f"Salom {name}")
+    elif name == "Olim":
+        print(f"Salom {name}")
+print(f"Kod {len(names)} marta qaytarilgan")
+
+
+a = [11,13,17,19,23,29,31,37,41,43,47,49,51,53,57,59,61,67,71,73,77,79,83,89,91,97] # 2-misol
+for b in a:
+    cube = b**3
+    print(cube)
+
+
+
+a = int(input("Bugun necha kishi bilan suhbaylashdingiz: "))
+b = str(input("1-suhbatlashgan odamingiz kim edi: "))
+d = str(input("2-suhbatlashgan odamingiz kim edi: "))
+c = str(input("3-suhbatlashgan odamingiz kim edi: "))
+items = [b, d, c]
+for x in items:
+    print(items)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
